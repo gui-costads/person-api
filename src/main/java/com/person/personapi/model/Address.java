90a59acc1fab0183ad5fee2da3ae66cd86503f73
Guilherme Costa <guilhermecosta.ds1@gmail.com>
2023-01-12 16:45:07 -0300

    Add model Address and Person

---
+++ src/main/java/com/person/personapi/model/Address.java
@@ -0,0 +1,79 @@
+package com.person.personapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "address_id", nullable = false)
+    private Long id;
+
+    private String street;
+    private String number;
+    private String city;
+    private String postalCode;
+    @ManyToOne
+    @JoinColumn(name = "person_id", nullable = false)
+    private Person person;
+
+    public Address() {
+    }
+
+    public Address(Long id, String street, String number, String city, String postalCode, Person person) {
+        this.id = id;
+        this.street = street;
+        this.number = number;
+        this.city = city;
+        this.postalCode = postalCode;
+        this.person = person;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+}
---
+++ src/main/java/com/person/personapi/model/Person.java
@@ -0,0 +1,57 @@
+package com.person.personapi.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "persons")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "person_id", nullable = false)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "birth", nullable = false)
+
+    private LocalDate birth;
+    @Column(name = "address")
+    @OneToMany(mappedBy = "person")
+    private List<Address> address;
+
+    public Person() {
+    }
+
+    public Person(Long id, String name, LocalDate birth, List<Address> address) {
+        this.id = id;
+        this.name = name;
+        this.birth = birth;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
+
+    public void setBirth(LocalDate birth) {
+        this.birth = birth;
+    }
+}
