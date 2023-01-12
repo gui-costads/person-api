2aed160cfbab5e5af6b0363ee14e4ba47cc7b109
Guilherme Costa <guilhermecosta.ds1@gmail.com>
2023-01-12 18:34:52 -0300

    Add repository Person

---
+++ src/main/java/com/person/personapi/repository/PersonRepository.java
@@ -0,0 +1,7 @@
+package com.person.personapi.repository;
+
+import com.person.personapi.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
