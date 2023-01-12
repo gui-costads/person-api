edbec147daed4746b7abb5ace9d3ce21b481425c
Guilherme Costa <guilhermecosta.ds1@gmail.com>
2023-01-12 19:28:25 -0300

    Update Person Mapper and add PersonCrate mapper

--- src/main/java/com/person/personapi/mapper/PersonMapper.java
+++ src/main/java/com/person/personapi/mapper/PersonMapper.java
@@ -1,6 +1,7 @@
 package com.person.personapi.mapper;
 
 import com.person.personapi.model.Person;
+import com.person.personapi.personDTO.PersonCreateDTO;
 import com.person.personapi.personDTO.PersonDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -11,5 +12,13 @@ public class PersonMapper {
 
     public Person personDtoToPerson(PersonDTO personDto){
         return MODEL_MAPPER.map(personDto, Person.class);
+    }
+
+    public Person personCreateDtoToPerson(PersonCreateDTO personCreateDTO){
+        return  MODEL_MAPPER.map(personCreateDTO, Person.class);
+    }
+
+    public PersonDTO personToPersonDto(Person person) {
+        return MODEL_MAPPER.map(person, PersonDTO.class);
     }
 }
