cccded4b0e21fc172d8d5af72e7a64bb8e5e2d3e
Guilherme Costa <guilhermecosta.ds1@gmail.com>
2023-01-12 18:36:13 -0300

    Add DTO pattern class PersonDTO

---
+++ src/main/java/com/person/personapi/personDTO/PersonDTO.java
@@ -0,0 +1,28 @@
+package com.person.personapi.personDTO;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.time.LocalDate;
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PersonDTO {
+
+    private String name;
+    private LocalDate birth;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
+
+    public void setBirth(LocalDate birth) {
+        this.birth = birth;
+    }
+}
