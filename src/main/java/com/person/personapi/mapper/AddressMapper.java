a9619040bf367171e26f8fbda3be79bd933cf08f
Guilherme Costa <guilhermecosta.ds1@gmail.com>
2023-01-13 00:13:28 -0300

    Add Mapper to Address

---
+++ src/main/java/com/person/personapi/mapper/AddressMapper.java
@@ -0,0 +1,31 @@
+package com.person.personapi.mapper;
+
+import com.person.personapi.dto.AddressCreateDTO;
+import com.person.personapi.dto.AddressDTO;
+import com.person.personapi.model.Address;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class AddressMapper {
+    private static final ModelMapper MODEL_MAPPER = new ModelMapper();
+
+    public Address addressDtoToAddress(AddressDTO addressDTO){
+        return MODEL_MAPPER.map(addressDTO, Address.class);
+    }
+
+    public List<Address>  addressListDtoToAddressList(List<AddressDTO> addressDTOList){
+        return addressDTOList.stream().map(this::addressDtoToAddress).collect(Collectors.toList());
+    }
+
+    public AddressDTO addressToAddressDto(Address address){
+        return MODEL_MAPPER.map(address, AddressDTO.class);
+    }
+
+    public Address addressCreateDtoToAddress(AddressCreateDTO addressCreateDTO){
+        return MODEL_MAPPER.map(addressCreateDTO, Address.class);
+    }
+}
