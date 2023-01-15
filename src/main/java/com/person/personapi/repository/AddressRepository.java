c859d1887b796947f1a24d0dc637e243f19c77d6
Guilherme Costa <guilhermecosta.ds1@gmail.com>
2023-01-14 23:56:17 -0300

    Update repository of address entity

--- src/main/java/com/person/personapi/repository/AddressRepository.java
+++ src/main/java/com/person/personapi/repository/AddressRepository.java
@@ -1,8 +1,11 @@
 package com.person.personapi.repository;
 
 import com.person.personapi.model.Address;
+import com.person.personapi.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
